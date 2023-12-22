b5f7c89f8a4354df4af3c0cf6684110f077d33c5
iammuskan-git <muskanhamal2000@gmail.com>
2023-12-22 16:50:53 +0545

    ADd Form

---
+++ src/api/muncipality/routes/muncipality.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * muncipality router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::muncipality.muncipality');
